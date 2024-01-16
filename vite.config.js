ee1eb2a9312dcd0e8b7ddd9d118b39f172ada562
ColinChiu87 <colinchiu87@gmail.com>
2024-01-16 10:34:48 +0800

    Complete!

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/3d_portfolio/',
+  plugins: [react()],
+  assetsInclude: ['**/*.glb']
+})
